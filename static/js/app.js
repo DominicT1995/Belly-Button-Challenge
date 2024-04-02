bed67cab0f9639340ad1f53078fad40afacfbc1e
DominicT1995 <domedt@gmail.com>
2024-04-02 15:13:52 -0500

    createBarchart function finalized, init and function structure corrected, optionChanged function created to handle dropdown changes

--- static/js/app.js
+++ static/js/app.js
@@ -2,47 +2,74 @@ const url = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json"
 
 const samplesData = d3.json(url);
 
-samplesData.then(function(data) {
+function init() {
 
-    //LIST POPULATION/////////////////////////
-    //console.log(data.names);
+    samplesData.then(function(data) {
 
-    let names = data.names;
+        let test = data.samples;
+        let test2 = test.filter(sample => sample.id == 940);
+        console.log(test2[0].sample_values)
 
-    dropDown = d3.select("#selDataset");
+        //LIST POPULATION/////////////////////////
+        //console.log(data.names);
 
-    for (let i = 0; i < names.length; i++) {
+        let dropDownMenu = d3.select("#selDataset");
 
-        dropDown.append("option").text(names[i]).property("value", names[i]);
-    }
+        let names = data.names;
 
-    console.log(d3.select("option").text());
-    console.log(d3.select("option").attr('value'));
+        for (let i = 0; i < names.length; i++) {
 
-    // BAR CHART //////////////////////////////
-    let samples = data.samples;
+            dropDownMenu.append("option").text(names[i]).property("value", names[i]);
+        }
 
-    //console.log(samples[0]);
+        //console.log(d3.select("option").text());
+        //console.log(d3.select("option").attr('value'));
 
-    let sampleValues = samples[0].sample_values.slice(0, 10).reverse()
-    let otuIds = samples[0].otu_ids.slice(0, 10).reverse();
-    let otuLabels = samples[0].otu_labels.slice(0,10).reverse();
+        let firstSubject = names[0];
 
-    let fixed = otuIds.map(id => "OTU " + id);
+        createBarchart(firstSubject);
 
-    let trace1 = {
-        x: sampleValues,
-        y: fixed,
-        text: otuLabels,
-        type: "bar",
-        orientation: "h"
-    };
+    });
+}
 
-    let chartData = [trace1];
+// BAR CHART //////////////////////////////
+function createBarchart(subject) {
 
-    Plotly.newPlot("bar", chartData);
-    //BAR CHART //////////////////////////////////
+    samplesData.then(function(data) {
+
+        let samples = data.samples;
+
+        let sampleCurrent = samples.filter(sample => sample.id == subject);
 
 
-});
+        let sampleValues = sampleCurrent[0].sample_values.slice(0, 10).reverse()
+        let otuIds = sampleCurrent[0].otu_ids.slice(0, 10).reverse();
+        let otuLabels = sampleCurrent[0].otu_labels.slice(0,10).reverse();
 
+        let otuIdsfinal = otuIds.map(id => "OTU " + id);
+
+        let trace1 = {
+            x: sampleValues,
+            y: otuIdsfinal,
+            text: otuLabels,
+            type: "bar",
+            orientation: "h"
+        };
+
+        let barchartData = [trace1];
+
+        Plotly.newPlot("bar", barchartData);
+
+    });
+}
+
+//Dropdown change function
+function optionChanged(subject) {
+
+    console.log("Showing results for Subject ID No.", subject);
+
+    createBarchart(subject);
+
+}
+
+init();
